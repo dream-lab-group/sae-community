60b30a50498ac6d217c4c5ffc3890e6f2e375893
abeerance <64221366+abeerance@users.noreply.github.com>
2022-05-07 11:29:25 +0200

    added react-query, next-auth, adjusted global theme, created login blueprint, adjusted app, deleted unneeded module.css (#4)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const appTheme = createTheme({
+  typography: {
+    fontFamily: `'Karla', sans-serif`,
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightBold: 700,
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider>
+      <ThemeProvider theme={appTheme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
