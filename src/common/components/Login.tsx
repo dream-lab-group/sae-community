135e2263246612163fa19bd9c9ecfef6e5931dba
abeerance <hadrian.chio@gmail.com>
2022-05-07 17:16:28 +0200

    adjusted login to grid layout

--- src/common/components/Login.tsx
+++ src/common/components/Login.tsx
@@ -1,43 +1,59 @@
-import { Box, TextField, Typography } from "@mui/material";
+import { Button, Grid, TextField, Typography } from "@mui/material";
 
 export const Login = () => {
   return (
-    <Box
+    <Grid
+      container
+      justifyContent="center"
+      alignItems="center"
       sx={{
         width: "auto",
         height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
         background: "#0f1626",
       }}
     >
-      <Box
+      <Grid
+        container
+        item
+        direction={"column"}
         sx={{
-          display: "flex",
-          flexDirection: "column",
           background: "#f5f5f5",
           paddingY: "40px",
           paddingX: "60px",
           borderRadius: "10px",
+          maxWidth: "600px",
         }}
       >
-        <Typography variant="h2" sx={{ color: "#ff533d", fontWeight: 700 }}>
-          Welcome Back!
-        </Typography>
-        <TextField
-          id="username"
-          label="Username"
-          variant="standard"
-          sx={{ marginTop: "20px" }}
-        />
-        <TextField
-          id="password"
-          label="Password"
-          variant="standard"
-          sx={{ marginTop: "20px" }}
-        />
-      </Box>
-    </Box>
+        <Grid item>
+          <Typography
+            variant="h2"
+            sx={{ color: "#ff533d", fontWeight: 700, lineHeight: 0.7 }}
+          >
+            Welcome Back!
+          </Typography>
+        </Grid>
+        <Grid item xs>
+          <TextField
+            id="username"
+            label="Username"
+            variant="standard"
+            sx={{ marginTop: "20px", witdh: "100%" }}
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            id="password"
+            label="Password"
+            variant="standard"
+            sx={{ marginTop: "20px" }}
+          />
+        </Grid>
+        <Grid item sx={{ marginTop: "30px" }}>
+          <Button variant="contained" color="error">
+            Login
+          </Button>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
