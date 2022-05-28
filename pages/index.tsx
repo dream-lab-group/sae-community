680fcef09baf646c02bdb761b3821f7db96f524b
abeerance <hadrian.chio@gmail.com>
2022-05-28 23:48:12 +0200

    refactored layout pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
 import { Directus } from '@directus/sdk';
-import { createTheme, ThemeProvider } from '@mui/material';
+import {
+  createTheme,
+  Grid,
+  ThemeProvider,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
 import type { NextPage } from 'next';
 import { useEffect, useState } from 'react';
+import Layout from '../common/components/layout';
+import { PageNavigation } from '../common/components/page-navigation/page-navigation';
+import { ProjectCard } from '../common/components/project-card/project-card';
 import SignIn from './signin';
 
 export const directus = new Directus('https://www.whatthebre.com/');
@@ -34,24 +43,75 @@ const appTheme = createTheme({
   },
 });
 
-const Home: NextPage = () => {
+const Home = () => {
   // force app to rehydrate after login to match the original HTML
   // hasMounted, to false. While it's false, doesn't bother rendering the "real" content.
-  const [hasMounted, setHasMounted] = useState(false);
+  // const [hasMounted, setHasMounted] = useState(false);
 
   // Inside the useEffect immediately trigger a re-render, setting hasMounted to true. While  true, the "real" content gets rendered.
   // useEffect only triggers after the component has mounted
-  useEffect(() => {
-    setHasMounted(true);
-  }, []);
-  if (!hasMounted) {
-    return null;
-  }
+  // useEffect(() => {
+  //   setHasMounted(true);
+  // }, []);
+  // if (!hasMounted) {
+  //   return null;
+  // }
+
+  const theme = useTheme();
+  const smBreakpointDown = useMediaQuery(theme.breakpoints.down('sm'));
+  const lgBreakpointUp = useMediaQuery(theme.breakpoints.up('lg'));
 
   return (
     // @ts-expect-error: Todo
-    <ThemeProvider theme={appTheme}>{!token && <SignIn />}</ThemeProvider>
+    <ThemeProvider theme={appTheme}>
+      <PageNavigation />
+      <Grid
+        container
+        width="100%"
+        maxWidth="2290px"
+        paddingY="40px"
+        paddingX={`${
+          smBreakpointDown ? '20px' : lgBreakpointUp ? '120px' : '42px'
+        }`}
+        spacing={{ sm: 4, md: 5, lg: 5 }}
+        columns={{ sm: 12, md: 3, lg: 3 }}
+      >
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+      </Grid>
+    </ThemeProvider>
   );
+};
+
+Home.getLayout = function getLayout(page: typeof Home) {
+  // @ts-expect-error: Todo
+  return <Layout>{page}</Layout>;
 };
 
 export default Home;
